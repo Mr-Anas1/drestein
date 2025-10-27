fe2ff46fd17e98a19d2231952c1a3821c92240be
Mr-Anas1 <a3689211@gmail.com>
2025-10-27 11:45:22 +0530

    Fixed some layout issues

--- src/constants/departments.js
+++ src/constants/departments.js
@@ -1,4 +1,6 @@
 export const DEPARTMENTS = [
+        { id: 'COMMON', name: 'Common Across Departments', short: 'COMMON', code: 'COMMON', image: '/common.jpg' },
+
     { id: 'AI-DS', name: 'Artificial Intelligence and Data Science', short: 'AI-DS', code: 'AI-DS', image: '/aids.jpeg' },
     { id: 'AI-ML', name: 'Artificial Intelligence and Machine Learning', short: 'AI-ML', code: 'AI-ML', image: '/aiml.jpeg' },
     { id: 'AGRI', name: 'Agricultural Engineering', short: 'AGRI', code: 'AGRI', image: '/agri.jpeg' },
@@ -8,7 +10,7 @@ export const DEPARTMENTS = [
     { id: 'CSE', name: 'Computer Science and Engineering', short: 'CSE', code: 'CSE', image: '/cse.jpeg' },
     { id: 'CSE-CYB', name: 'Computer Science and Engineering (Cyber Security)', short: 'CSE-CYB', code: 'CYB', image: '/cyber.jpeg' },
     { id: 'CSE-IOT', name: 'Computer Science and Engineering (Internet of Things)', short: 'CSE-IOT', code: 'IOT', image: '/iot.jpeg' },
-    { id: 'IT', name: 'Information Technology', short: 'IT', code: 'IT', image: '/it.jpeg' },
+    { id: 'IT', name: 'Information Technology', short: 'IT', code: 'IT', image: '/IT.webp' },
     { id: 'ECE', name: 'Electronics and Communication Engineering', short: 'ECE', code: 'ECE', image: '/ece.jpeg' },
     { id: 'EEE', name: 'Electrical and Electronics Engineering', short: 'EEE', code: 'EEE', image: '/eee.jpeg' },
     { id: 'EIE', name: 'Electronics and Instrumentation Engineering', short: 'EIE', code: 'EIE', image: '/eie.jpeg' },
@@ -16,7 +18,6 @@ export const DEPARTMENTS = [
     { id: 'MED-ELE', name: 'Medical Electronics Engineering', short: 'MED-ELE', code: 'MED', image: '/med-ele.jpeg' },
     { id: 'MBA', name: 'Master of Business Administration', short: 'MBA', code: 'MBA', image: '/mba.jpg' },
     { id: 'S&H', name: 'Science and Humanities', short: 'S&H', code: 'S&H', image: '/s&h.jpg' },
-    { id: 'COMMON', name: 'Common Across Departments', short: 'COMMON', code: 'COMMON', image: '/common.jpg' },
 ];
 
 export const getDepartmentById = (id) => {
