90c6cecd5a4df908a65878bfed4b185696e95e57
Mr-Anas1 <a3689211@gmail.com>
2026-01-04 16:14:58 +0530

    MBA Workshop, added sdg goals

--- src/components/MbaHeader.jsx
+++ src/components/MbaHeader.jsx
@@ -8,17 +8,17 @@ export default function MbaHeader() {
     return (
         <div className="flex justify-between items-center px-4 md:px-6 h-16 border-b border-gray-600">
 
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-1 sm:gap-2 md:gap-3">
                 <button
                     type="button"
                     onClick={() => router.push("/mba")}
-                    className="w-[160px] sm:w-[180px] md:w-[200px] lg:w-[250px]"
+                    className="w-[120px] sm:w-[180px] md:w-[200px] lg:w-[250px]"
                     aria-label="Go to MBA page"
                 >
                     <img src="/logo.png" alt="saveetha-logo" loading="lazy" decoding="async" />
                 </button>
 
-                <div className="w-[40px] sm:w-[50px] md:w-[60px] lg:w-[80px] flex-shrink-0">
+                <div className="w-[40px] sm:w-[50px] md:w-[60px] flex-shrink-0">
                     <img
                         src="/excellence.png"
                         alt="excellence-logo"
@@ -33,8 +33,30 @@ export default function MbaHeader() {
                     onClick={() => router.push("/mba")}
                     className="font-audiowide text-xl md:text-2xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent"
                 >
-                    MBA
+                    DoMS
                 </button>
+
+                <div className="w-[40px] sm:w-[50px]   flex-shrink-0">
+                    <img
+                        src="/mba/sdg-04.png"
+                        alt="sdg-logo"
+                        className="w-full h-auto object-contain"
+                        loading="lazy"
+                        decoding="async"
+                    />
+                </div>
+
+
+                <div className="w-[40px] sm:w-[50px]   flex-shrink-0">
+                    <img
+                        src="/mba/sdg-09.png"
+                        alt="sdg-09-logo"
+                        className="w-full h-auto object-contain"
+                        loading="lazy"
+                        decoding="async"
+                    />
+                </div>
+
             </div>
 
             <div className="flex items-center gap-3" />
